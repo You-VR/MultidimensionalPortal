45b37d9acfbfc6fe0b0c93db17b08bc32412ab75
youvr <32546077+youvr@users.noreply.github.com>
2017-10-27 18:28:19 +0200

    First Commit

---
+++ Assets/VonderPortal/Scripts/Dimension.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dimension : MonoBehaviour {
+
+    public string dimensionName;
+    public Scene scene;
+    private SceneType sceneType;
+
+    int sceneLayer { get
+        {
+            switch (sceneType)
+            {
+                case SceneType.last:
+                    return LayerMask.NameToLayer("LastScene");
+                case SceneType.current:
+                    return LayerMask.NameToLayer("CurrentScene");
+                case SceneType.next:
+                    return LayerMask.NameToLayer("NextScene");
+                default:
+                    Debug.LogError("Scene layers not set up");
+                    return LayerMask.NameToLayer("Default");
+            }
+        }
+    }
+
+    public List<GameObject> rootObjects;
+
+    public enum SceneType
+    {
+        last,
+        current,
+        next
+    }
+
+
+
+    public void LoadScene( SceneType _sceneType) {
+        sceneType = _sceneType;
+
+        if ( !scene.isLoaded)
+        {
+            StartCoroutine(LoadSceneAndInit());
+        } else
+        {
+            pruneSceneAndSetLayers();
+        }        
+    }
+
+    public void UnloadScene()
+    {
+        SceneManager.UnloadSceneAsync(scene);
+    }
+
+
+    IEnumerator LoadSceneAndInit()
+    {
+        AsyncOperation asyncLoad;
+        
+        asyncLoad = SceneManager.LoadSceneAsync(dimensionName, LoadSceneMode.Additive);
+        if (asyncLoad.isDone)
+        {
+            pruneSceneAndSetLayers();
+        } else
+        {
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+            pruneSceneAndSetLayers();
+        }
+
+    }
+    void pruneSceneAndSetLayers()
+    {
+        scene = SceneManager.GetSceneByName(dimensionName);
+        GameObject[] rootObjects = scene.GetRootGameObjects();
+        foreach (GameObject rootObject in rootObjects)
+        {
+            if (rootObject.name == "Persistant")
+            {
+                UnityEngine.Object.Destroy(rootObject);
+            }
+            else
+            {
+                Transform[] childrenTransforms = rootObject.GetComponentsInChildren<Transform>();
+                foreach (Transform t in childrenTransforms)
+                {
+                    t.gameObject.layer = sceneLayer;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/VonderPortal/Scripts/DimensionManager.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+class ActiveDimensions
+{
+    private Dimension[] activeDimensions = new Dimension[3];
+
+    public Dimension lastDimension { get { return activeDimensions[0]; } }
+    public Dimension currDimension { get { return activeDimensions[1]; } }
+    public Dimension nextDimension { get { return activeDimensions[2]; } }
+
+    public void set(Dimension[] dimensions)
+    {
+        var unloadDimensions = activeDimensions.Except(dimensions);
+        activeDimensions = dimensions;
+
+        foreach (Dimension unloadDimension in unloadDimensions)
+        {
+            if (unloadDimension != null) { unloadDimension.UnloadScene(); }
+        }
+
+        if (activeDimensions[0] != null) { activeDimensions[0].LoadScene(Dimension.SceneType.last); }
+        if (activeDimensions[1] != null) { activeDimensions[1].LoadScene(Dimension.SceneType.current); }
+        if (activeDimensions[2] != null) { activeDimensions[2].LoadScene(Dimension.SceneType.next); }
+    }
+}
+
+
+public class DimensionManager : MonoBehaviour {
+
+    public string[] dimension_names;
+
+    private List<Dimension> dimensions;
+    private ActiveDimensions activeDimensions;
+    private int dimensionIndex;
+
+    public delegate void ChangeDimensionHandler(int dimensionIndex);
+    public static event ChangeDimensionHandler ChangeDimension;
+
+    void OnEnable()
+    {
+        ChangeDimension += ChangeLoadedDimensions;
+    }
+
+
+    void OnDisable()
+    {
+        ChangeDimension -= ChangeLoadedDimensions;
+    }
+
+    private void Awake()
+    {
+        // Validate List of Scene names
+        List<string> scenesInBuild = new List<string>();
+        for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            int lastSlash = scenePath.LastIndexOf("/");
+            scenesInBuild.Add(scenePath.Substring(lastSlash + 1, scenePath.LastIndexOf(".") - lastSlash - 1));
+        }
+
+        dimensions = new List<Dimension>();
+        foreach (string dimension_name in dimension_names)
+        {
+            if (!scenesInBuild.Contains(dimension_name))
+            {
+                Debug.LogError("Scene not found: " + dimension_name);
+            } else
+            {
+                Dimension newDimension = this.gameObject.AddComponent<Dimension>();
+                newDimension.dimensionName = dimension_name;
+                dimensions.Add(newDimension);
+            }                
+        }
+
+        activeDimensions = new ActiveDimensions();
+    }
+
+    // Use this for initialization
+    void Start () {
+        ChangeDimension(1) ;
+
+    }
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Click"))
+        {
+            if (ChangeDimension != null)
+                ChangeDimension(dimensionIndex + 1);
+        }
+    }
+
+    void ChangeLoadedDimensions(int _dimensionIndex)
+    {
+        if(_dimensionIndex <= dimensions.Count)
+        {
+            dimensionIndex = _dimensionIndex;
+            assignDimensions();
+        } else
+        {
+            Debug.LogError("Invalid dimension index: " + dimensionIndex);
+        }
+
+    }
+
+    void assignDimensions()
+    {
+        // Check dimension index is valid
+        if(dimensionIndex < 0 || dimensionIndex > dimensions.Count)
+        {
+            Debug.LogError("Invalid dimension index: " + dimensionIndex);
+            return;
+        }
+
+        switch (dimensions.Count)
+        { 
+            case 1:
+                activeDimensions.set(new Dimension[] {  null, dimensions[0], null  });
+                break;
+            case 2:
+                // First dimension
+                if (dimensionIndex == 1)
+                {
+                    activeDimensions.set(new Dimension[] { null, dimensions[0], dimensions[1] });
+                }
+                // Last dimension
+                else
+                {
+                    activeDimensions.set(new Dimension[] { dimensions[0], dimensions[1], null });
+                }
+                break;
+            default:
+                // First dimension
+                if (dimensionIndex == 1)
+                {
+                    activeDimensions.set(new Dimension[] { null, dimensions[0], dimensions[1] });
+                }
+                // Last dimension
+                else if (dimensionIndex == dimensions.Count)
+                {
+                    activeDimensions.set(new Dimension[] { dimensions[dimensionIndex - 2], dimensions[dimensionIndex - 1], null });
+                }
+                // Middle dimension
+                else
+                {
+                    activeDimensions.set(new Dimension[] { dimensions[dimensionIndex - 2], dimensions[dimensionIndex - 1], dimensions[dimensionIndex] });
+                }
+                break;
+        }
+    }
+}
