4c62dcda7f848e6d78d936d5258a561225e9552f
youvr <32546077+youvr@users.noreply.github.com>
2017-11-21 20:47:40 +0100

    Commit

---
+++ Assets/VonderPortal/Editor/StageGizmoLauncher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+class StageGizmoLauncher : Editor
+{
+    static GameObject stageGizmo;
+    static StageGizmoLauncher()
+    {
+        EditorApplication.update += makeStageGizmo;
+    }
+
+    static void makeStageGizmo()
+    {
+        if (    StageGizmo.stageGizmo == null && 
+                stageGizmo == null && 
+                EditorApplication.isPlaying == false && 
+                FindObjectsOfType(typeof(StageGizmo)).Length == 0) {
+
+           stageGizmo = (GameObject)PrefabUtility.InstantiatePrefab(Resources.Load("[StageGizmo]"));
+           //stageGizmo.hideFlags = HideFlags.HideInInspector;
+        }        
+    }
+}
